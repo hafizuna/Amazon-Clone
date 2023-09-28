4d613b970f21c6200579e87ef1cdb36bb2eedeb6
hafizuna <hafizyt2014@gmail.com>
2023-09-28 23:41:15 +0300

    Styled the cart page and connected to firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBwuK4CwL8Pnmiww1cKTauQIOaGNyxaH68",
+  authDomain: "clone-295fd.firebaseapp.com",
+  projectId: "clone-295fd",
+  storageBucket: "clone-295fd.appspot.com",
+  messagingSenderId: "318938551601",
+  appId: "1:318938551601:web:90e4f72b2483c032fc8c10",
+  measurementId: "G-WKTHTRD433"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
